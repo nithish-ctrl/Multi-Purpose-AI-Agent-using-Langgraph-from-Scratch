405e386b25137d19ad669f298530e3c4d3c5a238
Nithish <nithish221b@gmail.com>
2026-06-14 22:39:03 +0530

    Changes to system prompt

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from langgraph.prebuilt import ToolNode
 from Tools import Search_engine
 from dotenv import load_dotenv
 from langchain_google_genai import ChatGoogleGenerativeAI
+from Prompt_template import System_prompt
 
 
 load_dotenv()
@@ -18,18 +19,8 @@ class AgentState(TypedDict):
 
 
 def Process_agent(state : AgentState) -> AgentState:
-    system_prompt = """You DO NOT have access to the internet.
-                        The ONLY way to access current information is by calling Search_engine.
-                        If a question requires current or live information,
-                        After receiving tool results, use them to answer the user.
-                        Do NOT search again unless the results are insufficient.
-                        CRITICAL SEARCH RULES:
-                        1. Optimize Keywords: Never search using full conversational sentences. Strip out filler words and convert user questions into concise, search-engine-friendly keywords.
-                        2. Triangulate Data: If a query requires both historical context and current events, use Wikipedia first for background, then DuckDuckGo for the latest updates.
-                        3. Citations: If the tool returns source URLs, always append clean, clickable Markdown links at the bottom of your final response. Never make up links.
-                        If the search results do not contain the answer, tell the user clearly instead of guessing."""
+    system_prompt = System_prompt
     
-     
     combined_messages = [
     SystemMessage(content=system_prompt),
     *state["messages"]
