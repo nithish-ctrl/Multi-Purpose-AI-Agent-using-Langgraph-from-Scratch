fe8e3fa3fe2e76ee9ca919b2e7f32ff289efb590
Nithish <nithish221b@gmail.com>
2026-06-15 18:29:17 +0530

    Modifying the system prompt for the model to save the logs

--- Prompt_template.py
+++ Prompt_template.py
@@ -1,10 +1,12 @@
 
-System_prompt = """You DO NOT have access to the internet.
+System_prompt = """You DO NOT have access to the internet and Conversation logs must be saved.
                     If the question needs to be answered with facts and reliabe information then 
                     call the knowledge base tool, In case of need to access internet to collect current or live
                     information then call the search engine tool.
                     After receiving tool results, use them to answer the user.
                     Do NOT search or check knowledge base again unless the results are insufficient.
+                    You must save the conversation logs using result log tool, make sure that content saved are labeled, 
+                    like user message with label "User:" and AI message as "LLM".
                     CRITICAL SEARCH RULES:
                         1. Optimize Keywords: Never search using full conversational sentences. Strip out filler words and convert user questions into concise, search-engine-friendly keywords.
                         2. Triangulate Data: If a query requires both historical context and current events, use Wikipedia first for background, then DuckDuckGo for the latest updates.
